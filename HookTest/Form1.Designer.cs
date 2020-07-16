2b3daf0ee02d3b21a981ba76d8cfb6ba0498d220
feltte <felt@ukezara.net>
2020-07-16 20:37:01 +0900

    プロジェクト ファイルを追加します。

---
+++ HookTest/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace HookTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox1.Location = new System.Drawing.Point(16, 51);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox1.MaxLength = 1024;
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(671, 495);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(16, 15);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(205, 29);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Hook Mouse";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(229, 15);
+			this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(205, 29);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Hook Keyboard";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(708, 562);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ HookTest/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HookTest
+{
+	public partial class Form1 : Form
+	{
+		#region Win32API Methods
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern IntPtr GetForegroundWindow();
+
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+		[System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+		private static extern IntPtr WindowFromPoint(Point point);
+		/*
+				[System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+				private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+				[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+				private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+				[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+				private static extern bool CloseHandle(IntPtr handle);
+		*/
+		#endregion
+
+		private readonly int windowTextBufferLength = 256;
+		private StringBuilder windowTextBuffer;
+		private readonly int logMessageBufferLength = 256;
+		private StringBuilder logMessageBuffer;
+
+		public Form1()
+		{
+			InitializeComponent();
+			windowTextBuffer = new StringBuilder(windowTextBufferLength);
+			logMessageBuffer = new StringBuilder(logMessageBufferLength);
+		}
+
+		private StreamWriter logStream;
+		private bool OpenLogFile()
+		{
+			try
+			{
+				logStream = new StreamWriter($"{DateTime.Now.ToString("yyyyMMdd-HHmm")}.txt", true, Encoding.GetEncoding("SHIFT_JIS"));
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		private void FlushLogFile() => logStream?.Flush();
+
+		private void CloseLogFile()
+		{
+			logStream?.Close();
+			logStream = null;
+		}
+
+		private (long, string) GetForgroundWindowHundleAndText()
+		{
+			long windowHundleInt64 = 0;
+			windowTextBuffer.Length = 0;
+			var hwnd = GetForegroundWindow();
+			if (hwnd != IntPtr.Zero)
+			{
+				GetWindowText(hwnd, windowTextBuffer, windowTextBuffer.Capacity);
+				windowHundleInt64 = hwnd.ToInt64();
+			}
+			return (windowHundleInt64, windowTextBuffer.ToString());
+		}
+
+		readonly MouseHook.Stroke strokeMask = MouseHook.Stroke.LEFT_DOWN | MouseHook.Stroke.LEFT_UP
+											| MouseHook.Stroke.RIGHT_DOWN | MouseHook.Stroke.RIGHT_UP;
+
+		void hookMouseTest(ref MouseHook.StateMouse s)
+		{
+			button1.Text = s.X + ", " + s.Y;
+			if ((s.Stroke & strokeMask) > 0)
+			{
+				logMessageBuffer.Length = 0;
+				(var hundle, var windowText) = GetForgroundWindowHundleAndText();
+				logMessageBuffer.Append($"{DateTime.Now:HH:mm:ss}, 0x{hundle:X8}, {windowText}, Mouse.{s.Stroke}");
+
+				logStream.WriteLine(logMessageBuffer);
+				textBox1.Text = logMessageBuffer + "\r\n" + textBox1.Text;
+			}
+		}
+
+		void hookKeyboardTest(ref KeyboardHook.StateKeyboard s)
+		{
+			button2.Text = s.Key.ToString();
+			logMessageBuffer.Length = 0;
+			(var hundle, var windowText) = GetForgroundWindowHundleAndText();
+			logMessageBuffer.Append($"{DateTime.Now:HH:mm:ss}, 0x{hundle:X8}, {windowText}, Keyboard");
+
+			logStream.WriteLine(logMessageBuffer);
+			textBox1.Text = logMessageBuffer + "\r\n" + textBox1.Text;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (logStream == null)
+			{
+				if (!OpenLogFile())
+				{
+					textBox1.Text = "failed to open log file." + textBox1.Text;
+				}
+			}
+			if (MouseHook.IsHooking)
+			{
+				MouseHook.Stop();
+				button1.Text = "Hook Mouse";
+				FlushLogFile();
+				return;
+			}
+
+			MouseHook.AddEvent(hookMouseTest);
+			MouseHook.Start();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (logStream == null)
+			{
+				if (!OpenLogFile())
+				{
+					textBox1.Text = "failed to open log file.\r\n" + textBox1.Text;
+				}
+			}
+			if (KeyboardHook.IsHooking)
+			{
+				KeyboardHook.Stop();
+				button2.Text = "Hook Keyboard";
+				FlushLogFile();
+				return;
+			}
+
+			KeyboardHook.AddEvent(hookKeyboardTest);
+			KeyboardHook.Start();
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			CloseLogFile();
+		}
+	}
+}
